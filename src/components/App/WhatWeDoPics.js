c24b1f0ccccfb033815479132bda4db6d2f72219
annie2527 <you@example.com>
2021-04-22 14:06:53 +0200

    banner

--- src/components/App/WhatWeDoPics.js
+++ src/components/App/WhatWeDoPics.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import translate from '../i18n/translate';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const WhatWeDoPics = () => {
+    useEffect(() => {
+        document.title = 'Muuras - What We Do'
+        Aos.init({duration: 1500});
+        Aos.refresh({duration: 1500});
+    }, [])
+
     return (
         <div className='item-img' id='pictures' data-aos='fade-right'>
         <section className='transition_img-container'>
