05a6f3fcf01005103b581dfad84600f644dd88c7
pjotrmuuras <90505508+pjotrmuuras@users.noreply.github.com>
2021-11-16 16:55:58 +0100

    Update Homepage.js

--- src/components/App/components/Homepage/Homepage.js
+++ src/components/App/components/Homepage/Homepage.js
@@ -4,12 +4,14 @@ import {Footer} from '../footer/Footer';
 import {Challenge} from './challenge/Challenge';
 import { SwampSection } from './swampSection/SwampSection';
 import { AMS } from './ams/AMS';
+import {dinsdag} from './dinsdag/dinsdag';
 
 export const Homepage = () => {
     return (
         <div>
             <Hero />
             <SwampSection />
+            <dinsdag />
             <Challenge />
             <AMS/>
             <Footer/>
