df812cafee4b2c9e61ecbcf0fc7d0a6bf2a68c73
pjotrmuuras <90505508+pjotrmuuras@users.noreply.github.com>
2021-12-01 17:35:55 +0100

    Update Sowflow.js

--- src/components/App/components/Homepage/sowflow/Sowflow.js
+++ src/components/App/components/Homepage/sowflow/Sowflow.js
@@ -1,1 +1,39 @@
+import React, {useEffect} from 'react';
+import './sowflow.scss';
+import {Button} from '../../Button/Button';
+import {Link} from 'react-router-dom';
+import Aos from 'aos';
+import "aos/dist/aos.css";
+import translate from '../../../../i18n/translate';
 
+export const Sowflow = () => {
+
+    useEffect(() => {
+        Aos.init({duration: 1500});
+        Aos.refresh({duration: 1500});
+    }, []) 
+
+    return (
+        <div className='sowflow-container'>  
+            <div className='sowflow-info'>
+                <div className='sowflow-text' data-aos='fade-left'>
+                <h2>{translate('sowflow.title')}</h2>
+                    <p>{translate('sowflow.description')}</p>
+                    <div className='btn-container'>
+                        <Link to='/about'>
+                        <Button
+                            className='btn'
+                            buttonStyle='btn-primary'
+                            buttonSize='btn--lge'>
+                            {translate('sowflow.button')}
+                        </Button>
+                        </Link>
+                    </div>
+                </div>
+                <div className='sowflow-img' data-aos='fade-right'>
+                    <img src='./img/regieorgaan.png' alt='regieorgaan'/>
+                </div>
+            </div>
+        </div>
+    )
+}
