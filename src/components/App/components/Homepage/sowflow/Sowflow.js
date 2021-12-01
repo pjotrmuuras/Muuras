7bfa508ae03a9e885d0a86f15c6836247fafdb5c
pjotrmuuras <90505508+pjotrmuuras@users.noreply.github.com>
2021-12-01 17:46:44 +0100

    Update Sowflow.js

--- src/components/App/components/Homepage/sowflow/Sowflow.js
+++ src/components/App/components/Homepage/sowflow/Sowflow.js
@@ -1,39 +1,34 @@
 import React, {useEffect} from 'react';
-import './sowflow.scss';
-import {Button} from '../../Button/Button';
-import {Link} from 'react-router-dom';
 import Aos from 'aos';
-import "aos/dist/aos.css";
+import "aos/dist/aos.css"
+import './sowflow.scss';
 import translate from '../../../../i18n/translate';
+import {Button} from '../../Button/Button';
 
 export const Sowflow = () => {
-
     useEffect(() => {
         Aos.init({duration: 1500});
         Aos.refresh({duration: 1500});
-    }, []) 
+    }, [])
 
     return (
-        <div className='sowflow-container'>  
-            <div className='sowflow-info'>
-                <div className='sowflow-text' data-aos='fade-left'>
-                <h2>{translate('sowflow.title')}</h2>
-                    <p>{translate('sowflow.description')}</p>
-                    <div className='btn-container'>
-                        <Link to='/about'>
+        <div className='sowflow-container'>
+            <div data-aos="fade-left" className='sowflow-img-container'>
+                <img src='./img/regieorgaan.png' alt='regieorgaan'/>
+            </div>
+            <div data-aos="fade-left" className='dinsdag-text-container'>
+                <h2>{translate('SowflowData.title')}</h2>
+                <h5>{translate('SowflowData.date')}</h5>
+                <p>{translate('SowflowData.description')}</p>
+                <a href="https://regieorgaan-sia.nl/financiering/kiem/" target='_blank' rel="noreferrer">
                         <Button
                             className='btn'
                             buttonStyle='btn-primary'
                             buttonSize='btn--lge'>
-                            {translate('sowflow.button')}
+                            {translate('SowflowData.button')}
                         </Button>
-                        </Link>
-                    </div>
-                </div>
-                <div className='sowflow-img' data-aos='fade-right'>
-                    <img src='./img/regieorgaan.png' alt='regieorgaan'/>
-                </div>
-            </div>
+                </a>
+            </div>        
         </div>
     )
 }
