350a7cfec29268a9a70ffbad081fae225e6b37a4
pjotrmuuras <90505508+pjotrmuuras@users.noreply.github.com>
2021-12-01 17:37:19 +0100

    Update Homepage.js

--- src/components/App/Homepage.js
+++ src/components/App/Homepage.js
@@ -1,4 +1,4 @@
-import {Sowflow} from './Sowflow/Sowflow';
+import {Sowflow} from './sowflow/Sowflow';
 import {Dinsdag} from './components/Homepage/dinsdag/Dinsdag';
 import { AMS } from './components/Homepage/ams/AMS';
 import {Hero} from './components/Homepage/hero/Hero'
