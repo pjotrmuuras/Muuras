a0d1e52a614d15506e56c8314123560a331eec26
pjotrmuuras <90505508+pjotrmuuras@users.noreply.github.com>
2025-02-11 21:31:14 +0100

    Update SwampSection.js
    Learn more buttom removed

--- src/components/App/components/Homepage/swampSection/SwampSection.js
+++ src/components/App/components/Homepage/swampSection/SwampSection.js
@@ -1,7 +1,6 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './swamp-section.scss';
-import {Button} from '../../Button/Button';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Aos from 'aos';
 import "aos/dist/aos.css";
 import translate from '../../../../i18n/translate';
@@ -9,31 +8,33 @@ import translate from '../../../../i18n/translate';
 export const SwampSection = () => {
 
     useEffect(() => {
-        Aos.init({duration: 1500});
-        Aos.refresh({duration: 1500});
-    }, []) 
+        Aos.init({ duration: 1500 });
+        Aos.refresh({ duration: 1500 });
+    }, []);
 
     return (
         <div className='swamp-container'>  
             <div className='swamp-info'>
                 <div className='swamp-text' data-aos='fade-left'>
-                <h2>{translate('swamp.title')}</h2>
+                    <h2>{translate('swamp.title')}</h2>
                     <p>{translate('swamp.description')}</p>
+                    {/* 
                     <div className='btn-container'>
                         <Link to='/about'>
-                        <Button
-                            className='btn'
-                            buttonStyle='btn-primary'
-                            buttonSize='btn--lge'>
-                            {translate('swamp.button')}
-                        </Button>
+                            <Button
+                                className='btn'
+                                buttonStyle='btn-primary'
+                                buttonSize='btn--lge'>
+                                {translate('swamp.button')}
+                            </Button>
                         </Link>
-                    </div>
+                    </div> 
+                    */}
                 </div>
                 <div className='swamp-img' data-aos='fade-right'>
                     <img src='./img/muuras-building2.jpg' alt='Muuras building 2'/>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
