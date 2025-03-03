b8f8ee808c8bf18fd06d39667d1d64e73d42e738
pjotrmuuras <90505508+pjotrmuuras@users.noreply.github.com>
2025-03-03 23:31:43 +0100

    Update Hero.js
    New try to have the backgroundvideo

--- src/components/App/components/Homepage/hero/Hero.js
+++ src/components/App/components/Homepage/hero/Hero.js
@@ -1,39 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './hero.scss';
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { Link } from 'react-router-dom';
 import { Button } from '../../Button/Button';
 import translate from '../../../../i18n/translate';
+import videoFile from './backgroundvideo.mp4';  // Import video
 
 export const Hero = () => {
-    useEffect(() => {
-        Aos.init({ duration: 1500 });
-        Aos.refresh();
-    }, []);
+  const [isShown, setIsShown] = useState(true);
 
-    return (
-        <div className='hero-container'>
-            {/* Background video */}
-            <video autoPlay loop muted className="hero-video">
-                <source src={require('./backgroundvideo.mp4')} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+    Aos.refresh({ duration: 1500 });
+  }, []);
 
-            <div className='text-container'>
-                <div className='text' data-aos='fade-right'>
-                    <h1>Muuras</h1>
-                    <h3>{translate('banner.description')}</h3>
-                </div>
+  return (
+    <div className="hero-container">
+      {/* Video background */}
+      <video autoPlay loop muted className="hero-video" preload="auto">
+        <source src={videoFile} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-                <div className='hero-btns'>
-                    <Link to='/contact'>
-                        <Button className='btn' buttonStyle='btn-primary' buttonSize='btn--lge'>
-                            {translate('banner.button1')}
-                        </Button>
-                    </Link>
-                </div>
-            </div>
+      <div className="text-container">
+        <div className="text" data-aos="fade-right">
+          <h1>Muuras</h1>
+          <h3>{translate('banner.description')}</h3>
         </div>
-    );
+
+        <div
+          className="hero-btns"
+          onMouseEnter={() => setIsShown(false)}
+          onMouseLeave={() => setIsShown(true)}
+        >
+          <Link to="/contact">
+            <Button
+              className="btn"
+              buttonStyle="btn-primary"
+              buttonSize="btn--lge"
+            >
+              {isShown ? translate('banner.button1') : translate('banner.button2')}
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
