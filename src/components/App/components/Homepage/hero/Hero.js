e938e30b797a0a1e530e53225bf9ba7a06b39ffe
pjotrmuuras <90505508+pjotrmuuras@users.noreply.github.com>
2025-03-03 22:17:56 +0100

    Update Hero.js
    Removed one of the buttons, clicking it now takes to the contact page.

--- src/components/App/components/Homepage/hero/Hero.js
+++ src/components/App/components/Homepage/hero/Hero.js
@@ -1,18 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect } from 'react';
 import './hero.scss';
 import Aos from "aos";
 import "aos/dist/aos.css";
-import {Link} from 'react-router-dom';
-import {Button} from '../../Button/Button';
+import { Link } from 'react-router-dom';
+import { Button } from '../../Button/Button';
 import translate from '../../../../i18n/translate';
 
 export const Hero = () => {
-    const [isShown, setIsShown] = useState(true);
-
     useEffect(() => {
-        Aos.init({duration: 1500});
-        Aos.refresh({duration: 1500});
-      }, []);
+        Aos.init({ duration: 1500 });
+        Aos.refresh();
+    }, []);
 
     return (
         <div className='hero-container'>
@@ -21,21 +19,15 @@ export const Hero = () => {
                     <h1>Muuras</h1>
                     <h3>{translate('banner.description')}</h3>
                 </div>
-           
-            <div className='hero-btns'
-                    onMouseEnter={() => setIsShown(false)}
-                    onMouseLeave={() => setIsShown(true)}
-                >
-            <Link to='/what-we-do'>
-                <Button
-                    className='btn'
-                    buttonStyle='btn-primary'
-                    buttonSize='btn--lge'>
-                        {isShown ? translate('banner.button1') : translate('banner.button2')}
-                </Button>
-            </Link>
-            </div>
+
+                <div className='hero-btns'>
+                    <Link to='/contact'>
+                        <Button className='btn' buttonStyle='btn-primary' buttonSize='btn--lge'>
+                            {translate('banner.button1')}
+                        </Button>
+                    </Link>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
