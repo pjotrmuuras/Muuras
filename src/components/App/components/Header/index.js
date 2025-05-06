85708c2fdb4d81b9fa09350919a068a002c518dc
pjotrmuuras <90505508+pjotrmuuras@users.noreply.github.com>
2025-05-06 22:29:01 +0200

    Update index.js

--- src/components/App/components/Header/index.js
+++ src/components/App/components/Header/index.js
@@ -26,7 +26,7 @@ const Header = () => {
             <div className='navbar-container'>
                 <div className='logo-container'>
                     <Link to='/' className='navbar-logo' onClick={closeMenuMobile}>
-                        <img src='img/logo-navbar.png' alt='' height={75}/>
+                        <img src='img/logo-navbar.png' alt='' />
                     </Link>
                 </div>
 
